386f3e575e6f7b20df3dd43790cba1108912ed63
SRW Code <141584178+srwcode@users.noreply.github.com>
2024-12-18 16:42:03 +0700

    Delete src directory

--- src/config.ts
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = "http://localhost:9000";
-export const Dashboard_URL = "http://localhost:9100";
-export const DEFAULT_IMAGE = "http://localhost:9000/upload/defaultImage.png";
-export const PerPage = 5;
