5faa3c5d39a350b6c6bf1b0af57ce1e2d923ecda
SRW Code <141584178+srwcode@users.noreply.github.com>
2024-12-18 16:40:28 +0700

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = "http://localhost:9000";
+export const Dashboard_URL = "http://localhost:9100";
+export const DEFAULT_IMAGE = "http://localhost:9000/upload/defaultImage.png";
+export const PerPage = 5;
